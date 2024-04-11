2688f21e3b9beb953c6810b1f218b7db6a1d6cbc
Andreas Schlemmer <andreas@schlemmer.io>
2024-04-11 08:40:16 +0200

    bump ffmpeg

--- ffmpeg-static.rb
+++ ffmpeg-static.rb
@@ -1,14 +1,14 @@
 class FfmpegStatic < Formula
-  version "6.1.1"
+  version "7.0"
 
   if Hardware::CPU.arm?
-    url "https://www.osxexperts.net/ffmpeg611arm.zip"
-    sha256 "1c69a9a4a5d0172d9aec611f19d282b38b3c3d69832621ecef87926335f62e0a"
+    url "https://www.osxexperts.net/ffmpeg7arm.zip"
+    sha256 "d2cd3145597a36ec17cb55273f1b7c2ce80def3b67e915293e649cd66281160c"
   end
 
   if Hardware::CPU.intel?
-    url "https://www.osxexperts.net/ffmpeg61intel.zip"
-    sha256 "9a7de6ea24052166340c50e00dd493da49315431e6bfde5ed2a600eab9f27b65"     
+    url "https://www.osxexperts.net/ffmpeg7intel.zip"
+    sha256 "2d01a9bb00c3d0d4a850baa12a9414af197c1199315443bce44064ffb8e4297a"     
   end
 
   def install 
--- ffprobe-static.rb
+++ ffprobe-static.rb
@@ -1,14 +1,14 @@
 class FfprobeStatic < Formula
-  version "6.1.1"
+  version "7.0"
 
   if Hardware::CPU.arm?
-    url "https://www.osxexperts.net/ffprobe611arm.zip"
-    sha256 "8ec78ce09c0b474ae7583908c8d79d801f754d8273dca3d5a5de51f307517fc7"     
+    url "https://www.osxexperts.net/ffprobe7arm.zip"
+    sha256 "bf0cccdda082ad7ed0d04cc6767014bce86f0a83bcd4a7b85137667ed0bcee0e"     
   end
 
   if Hardware::CPU.intel?
-    url "https://www.osxexperts.net/ffprobe61intel.zip"
-    sha256 "977381d8cec9c477fef4cdbb259addc7e5506e47fa61087e620d5ed01a7c7a51"     
+    url "https://www.osxexperts.net/ffprobe7intel.zip"
+    sha256 "e14a2feb619ee21cb96ac3bf8c34a5d57f2ea61c38fe9d89b1a084d619b6aebf"     
   end
 
   def install 
